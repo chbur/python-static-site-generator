283b4702f42fe07cc098efff776d861857e6fb44
chbur <chris.burzynski@gmail.com>
2021-01-06 11:24:53 -0500

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -1,4 +1,13 @@
 import re
-from yaml import load
-from yaml import FullLoader
+
+from yaml import load, FullLoader
 from collections.abc import Mapping
+
+
+class Content(Mapping):
+    __delimiter = r"^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
+
+    @classmethod
+    def load(cls, string):
+        
