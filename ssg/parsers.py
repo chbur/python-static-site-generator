979551e3dfac94371974e50b53b54ba0e2209ac9
chbur <chris.burzynski@gmail.com>
2021-01-06 12:21:58 -0500

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -36,4 +36,9 @@ class ResourceParser(Parser):
 class MarkdownParser(Parser):
     extensions = [".md", ".markdown"]
 
-    
+    def parse(self, path, source, dest):
+        content = Content.load(self.read(path))
+        html = markdown(content.body)
+        self.write(path, dest, html)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata:
+        {}\n".format(path.name, content))
