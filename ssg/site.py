c578a6f07f6a12f1feff265bc8a4775e942ded4b
chbur <chris.burzynski@gmail.com>
2021-01-06 10:34:24 -0500

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -15,10 +15,17 @@ class Site:
             if parser.valid_extension(extension):
                 return parser
 
-    
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+        if parser is not None:
+            parse.parse(path, self.source, self.dest)
+        else:
+            print("Not Implemented")
 
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
